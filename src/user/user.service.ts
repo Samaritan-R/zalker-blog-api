976191ee0648e218d3de901cfc936c49423d4178
18811583798@163.com <18811583798@163.com>
2025-01-03 11:27:50 +0800

    feat: 完成合并

---
+++ src/user/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  create(createUserDto: CreateUserDto) {
+    return {
+      msg: '用户创建成功',
+      data: createUserDto,
+    };
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+  getUserName() {
+    return '张三';
+  }
+}
